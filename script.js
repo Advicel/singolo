cfe16a8109b0621a38b16c3e65f236b58c494175
unknown <sowetnikov2010@yandex.ru>
2020-03-21 15:53:30 +0300

    fix: anchors->smooth anchors slider->smooth slider

--- script.js
+++ script.js
@@ -1,107 +1,172 @@
-let slideIndex = 1;
-const leftArrow = document.querySelector(".leftArrow");
+/*const leftArrow = document.querySelector(".leftArrow");
 const rightArrow = document.querySelector(".rightArrow")
-const slides = document.querySelectorAll(".slide");
-const portfolioButtons = document.querySelectorAll(".button");
-const verticalPhoneButton = document.querySelector(".buttonHomeVertical");
-const horizontalPhoneButton = document.querySelector(".buttonHomeHorizontal");
-const slide2PhoneButton = document.querySelector(".buttonHomeSlide2");
+const submitButton = document.querySelector(".send");*/
+
+
+/**tooglePhoneDisplay */
+
 const verticalPhoneDisplay = document.querySelector(".phoneVerticalDisplay");
 const horizontalPhoneDisplay = document.querySelector(".phoneHorizontalDisplay");
 const slide2PhoneDisplay = document.querySelector(".phoneSlide2Display");
 
-const PhoneDisplay = document.querySelectorAll(".phoneDisplay");
+const verticalPhoneButton = document.querySelector(".buttonHomeVertical");
+const horizontalPhoneButton = document.querySelector(".buttonHomeHorizontal");
+const slide2PhoneButton = document.querySelector(".buttonHomeSlide2");
+
+verticalPhoneButton.addEventListener("click",()=>togglePhoneDisplay(verticalPhoneDisplay));
+horizontalPhoneButton.addEventListener("click",()=>togglePhoneDisplay(horizontalPhoneDisplay));
+slide2PhoneButton.addEventListener("click",()=>togglePhoneDisplay(slide2PhoneDisplay));
+
+function togglePhoneDisplay(screen){
+    screen.classList.toggle("inline-flex");
+}
+
+
+/**swapImg */
+
+const mixRand = () => Math.random() - 0.5;
+
+const portfolioButtons = document.querySelectorAll(".button");
+
+for (const button of portfolioButtons) {
+    button.addEventListener('click',()=> swapImg(button));
+  }
+
+function swapImg(button){
+
+    [...portfolioButtons].forEach(button=>button.classList.remove("activePortfolioButton"));
+    button.classList.add("activePortfolioButton");
+
+    const arrImg=[...galleryImg];
+    arrImg.forEach(e=>e.style.border="none");
+    const arrImgSrcMix=arrImg.map(e=>e.src).sort((a,b) =>mixRand());
+    arrImg.map((e,i)=>e.src=arrImgSrcMix[i]);
+}
+
+/**setNavButtonActive */
 
 const navButtons = document.querySelectorAll(".navigation-link");
+
+navButtons.forEach(button => {
+    button.addEventListener('click',()=> setNavButtonActive(button));
+});
+
+function setNavButtonActive(button){
+    [...navButtons].forEach(button=>button.classList.remove("activeNavigationButton"));
+    button.classList.add("activeNavigationButton");
+}
+
+
+/**setGalleryImgActive */
+
 const galleryImg =document.getElementsByClassName('gallery-img'); 
-const submitButton = document.querySelector(".send");
-const mixRand=()=> {
-    return Math.random()-0.5;
-}
 
-
-for (const button of navButtons) {
-  button.addEventListener('click',()=> setNavButtonActive(button));
-}
-for (const button of portfolioButtons) {
-  button.addEventListener('click',()=> swapImg(button));
-}
 for(const img of galleryImg){
     img.addEventListener('click',()=>setGalleryImgActive(img))
 }
 
-
-leftArrow.addEventListener("click",()=>showSlides(slideIndex-=1));
-rightArrow.addEventListener("click",()=>showSlides(slideIndex+=1));
-verticalPhoneButton.addEventListener("click",()=>tooglePhoneDisplay(verticalPhoneDisplay));
-horizontalPhoneButton.addEventListener("click",()=>tooglePhoneDisplay(horizontalPhoneDisplay));
-slide2PhoneButton.addEventListener("click",()=>tooglePhoneDisplay(slide2PhoneDisplay));
-
-function showSlides(n){
-    if(n>slides.length) slideIndex = 1;
-    if(n<1) slideIndex=slides.length;
-    slides.forEach((slide,index)=>slides[index].style.display="none");
-    slides[slideIndex-1].style.display="flex";
-}
-function tooglePhoneDisplay(screen){
-    if(screen.style.display==="none"){
-        screen.style.display = "inline-flex";
-    } else {
-        screen.style.display="none";
-    }
-}
-function swapImg(button){
-    const arrImg=Array.from(galleryImg);
-    Array.from(portfolioButtons).map(e=>{e.style.color="#666d89",e.style.borderColor="#666d89"});
-    button.style.color="#c5c5c5";
-    button.style.borderColor="#c5c5c5";
-    arrImg.map(e=>e.style.border="none");
-    const arrImgSrcMix=arrImg.map(e=>e.src).sort((a,b) =>mixRand());
-    arrImg.map((e,i)=>e.src=arrImgSrcMix[i]);
-}
-function setNavButtonActive(button){
-    Array.from(navButtons).map(e=>e.style.color="white");
-    button.style.color = "#f06c64";
-}
 function setGalleryImgActive(img){
-    Array.from(galleryImg).map(e=>e.style.border="none");
+    [...galleryImg].map(e=>e.style.border="none");
     img.style.border="5px solid #F06C64";
 }
-function sendMessage(){
-    /*const fieldName = document.querySelector(".fieldName").value;
-    const fieldEmail = document.querySelector(".fieldEmail").value;*/
-    const fieldSubject = document.querySelector(".fieldSubject").value;
-    const fieldDecription = document.querySelector(".fieldDecription").value;
+
+
+/**sendMessage */
+
+function sendMessage(event){
+    event.preventDefault();
+    const fieldName = document.querySelector(".fieldName");
+    const fieldEmail = document.querySelector(".fieldEmail")
+    const fieldSubject = document.querySelector(".fieldSubject");
+    const fieldDecription = document.querySelector(".fieldDecription");
     
     let message="Письмо отправлено";
-    
-    if(fieldSubject=="") {
-        message+="\nБез темы";
-    }else{
-        message+="\nТема: "+fieldSubject;
-    }
 
-    if(fieldDecription==""){
-        message+="\nБез описания";
-    }else{
-        message+="\nОписание: "+fieldDecription;
-    }
+    message+=fieldSubject.value==""?"\nБез темы":"\nТема: "+fieldSubject.value;
+    message+=fieldDecription.value==""?"\nБез описания":"\nОписание: "+fieldDecription.value;
+    
     alert(message);
-    return true;
+
+    fieldName.value="";
+    fieldEmail.value="";
+    fieldSubject.value="";
+    fieldDecription.value="";
 }
 
 
+/*******************smooth anchors******************/
 function scrollToAnchor(element){
-    //document.querySelector(element).scrollIntoView({behavior: "smooth",block:"center"});
     const headerHeight = 95;
 
-    // scroll to your element
-    document.querySelector(element).scrollIntoView();
+    const scrolledY =document.querySelector(element).offsetTop;
 
-    // now account for fixed header
-    const scrolledY = window.scrollY;
-
-    if(scrolledY){
-        window.scroll(0, scrolledY - headerHeight);
-    }
+    window.scrollTo({top:scrolledY - headerHeight,behavior:"smooth"});
 }
+
+
+/****************************carousel*******************************/
+const items= document.querySelectorAll(".item");
+let currentItem = 0;
+let isEnabled=true;
+
+function changeCurrentItem(n){
+    currentItem=(n+items.length) % items.length;
+}
+
+function hideItem(direction){
+    isEnabled=false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener("animationend",function(){
+        this.classList.remove("active",direction);
+    })
+}
+
+function showItem(direction){
+    items[currentItem].classList.add("next",direction);
+    items[currentItem].addEventListener("animationend",function(){
+        this.classList.remove("next",direction);
+        this.classList.add("active");
+        isEnabled=true;
+    })
+}
+
+function previousItem(n){
+    hideItem("to-right")
+    changeCurrentItem(n-1);
+    showItem('from-left');
+}
+
+document.querySelector(".control.left").addEventListener("click",function(){
+    if(isEnabled){
+        previousItem(currentItem)
+    }
+});
+
+function nextItem(n){
+    hideItem("to-left")
+    changeCurrentItem(n+1);
+    showItem('from-right')
+}
+
+document.querySelector(".control.right").addEventListener("click",function(){
+    if(isEnabled){
+        nextItem(currentItem)
+    }
+});
+
+
+/*simple slider
+
+const slides = document.querySelectorAll(".slide");
+leftArrow.addEventListener("click",()=>showSlides(slideIndex-=1));
+//rightArrow.addEventListener("click",()=>showSlides(slideIndex+=1));
+function showSlides(n){
+    if(n>slides.length) {
+        slideIndex = 1;
+    }
+    if(n<1){ 
+        slideIndex=slides.length;
+    }
+    slides.forEach((slide,index)=>slides[index].style.display="none");
+    slides[slideIndex-1].style.display="flex";
+}*/
